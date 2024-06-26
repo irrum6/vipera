bf552b25b0b7451e4ca201a825c1d91856804959
irrum6 <irrum6@github.com>
2024-06-26 21:35:46 +0400

    template compiler

---
+++ tcompiler.js
@@ -0,0 +1,33 @@
+// template compiler
+const fs = require("fs");
+const fsAsync = require("fs").promises;
+
+let dir1 = "./components";
+let tplnames = [`${dir1}/colorbox`, `${dir1}/radiobox`];
+
+async function main() {
+
+    let indexFilename = "index3.html";
+    let outputFilename = "index.html";
+
+    let tplcompiled = "";
+
+    for (const tplname of tplnames) {
+        let filename = `${tplname}.html`;
+        let tplcontent = await fsAsync.readFile(filename);
+        tplcompiled = tplcompiled + tplcontent;
+    }
+
+    //is buffer
+    let indexFileContent = await fsAsync.readFile(indexFilename);
+
+    indexFileContent = indexFileContent.toString();
+
+    let replacePattern = "<replace-content></replace-content>";
+
+    let compiledContent = indexFileContent.replace(replacePattern, tplcompiled);
+
+    await fsAsync.writeFile(outputFilename, compiledContent);
+}
+
+main();
